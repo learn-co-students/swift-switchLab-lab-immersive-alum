fd4feb142a01883ebe77c96f1478d344f22c16c7
shirleyz9402 <shirleyz9402@yahoo.com>
2018-07-17 13:24:55 -0400

    done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -22,7 +22,12 @@ let b = 32
 /*: Question 1
 ### **(1)** Print "true" if a modulo b is equal to zero.
 */
-
+switch a % b {
+case 0:
+    print(true)
+default:
+    print(false)
+}
 
 
 
@@ -33,7 +38,12 @@ let b = 32
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
+switch y / Int(x) {
+case 0...2:
+    print(false)
+default:
+    print(true)
+}
 
 
 
@@ -44,6 +54,12 @@ let b = 32
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
 
+switch y > Int(x) && a / b > 9 {
+case true:
+    print(true)
+default:
+    print(false)
+}
 
 
 
@@ -56,7 +72,14 @@ let b = 32
 */
 
 
-
+func isGreater(a: Int, b: Int) -> String{
+    switch a > b{
+    case true:
+        return "yep"
+    case false:
+        return "nope"
+    }
+}
 
 
 
@@ -76,6 +99,14 @@ let b = 32
 
 
 
+func isForceWith(name: String) -> Bool {
+    switch name {
+    case "Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader":
+        return true
+    default:
+        return false
+    }
+}
 
 
 /*: Question 6
@@ -86,7 +117,18 @@ let b = 32
  isInRange(5) // prints "big"
  ```
 */
-
+func isInRange(arg: Int) {
+    switch arg {
+    case 0...3:
+        print("small")
+    case 4...6:
+        print("medium")
+    case 7...10:
+        print("big")
+    default:
+        print("not sure")
+    }
+}
 
 
 
